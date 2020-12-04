7e425bb11b8b5a6345d7eed7a8969482a77cb081
unknown <szakalyzs@gmail.com>
2020-12-04 17:59:24 +0100

    All done.

--- js/main.js
+++ js/main.js
@@ -40,3 +40,48 @@ console.log(hu.date(currDate));
 console.log(hu.date2(currDate));
 console.log(hu.currency(15420.5));
 console.log(hu.list(stringArr));
+
+//3. feladat----------------------------
+const objectConverter = {
+    arrayToMap(paramArr) {
+        const tempMap = new Map();
+        paramArr.forEach((item, index) => tempMap.set(index, item));
+        return tempMap;
+     },
+    arrayToSet(paramArr) {
+        const mySet = new Set();        
+        paramArr.forEach(item => mySet.add(item));
+        return mySet;
+    },
+    setToMap(paramSet) {
+        const tempMap = new Map();
+        paramSet.forEach(value => tempMap.set(value,value));
+        return tempMap;
+     },
+    setToArray(paramSet) {
+        const arr = [];
+        paramSet.forEach(item => arr.push(item));
+        return arr;
+    },
+    mapToArray(paramMap) {
+        const arr = [];
+        paramMap.forEach(value => arr.push(value));
+        return arr;
+     },
+    mapToSet(paramMap) { 
+        const tempSet = new Set();
+        paramMap.forEach(value => tempSet.add(value));
+        return tempSet;
+    },
+}
+const mySet = new Set();
+mySet.add('id1').add('id2').add('id3').add('id4');
+const myMap = new Map();
+myMap.set('first', 11).set('second', 22).set('third', 33).set('fourth', 44);
+
+console.log('setToArray: ',objectConverter.setToArray(mySet));
+console.log('mapToArray: ',objectConverter.mapToArray(myMap));
+console.log('arrayToSet: ',objectConverter.arrayToSet(array));
+console.log('mapToSet: ',objectConverter.mapToSet(myMap));
+console.log('setToMap: ', objectConverter.setToMap(mySet));
+console.log('arrayToMap: ',objectConverter.arrayToMap(array));
